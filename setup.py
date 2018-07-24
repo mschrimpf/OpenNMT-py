d0073ddf9c7a6dbae7f1627113d33a990c92e38c
WH <william2hartman@gmail.com>
2018-07-24 16:14:21 -0400

    dependency_links

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(name='OpenNMT-py',
       description='A python implementation of OpenNMT',
       version='0.2',
 
-      install_requires=['six', 'tqdm', 'torch>=0.4.0', 'torchvision', 'git+https://github.com/pytorch/text', 'future'],
+      dependency_links=['https://github.com/pytorch/text/master/tarball'],
+      install_requires=['six', 'tqdm', 'torch>=0.4.0', 'torchvision', 'text', 'future'],
 
       packages=['onmt', 'onmt.encoders', 'onmt.modules', 'onmt.tests',
                 'onmt.translate', 'onmt.decoders', 'onmt.inputters',
